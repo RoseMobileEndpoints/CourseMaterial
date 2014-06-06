440d6be8d80f8c1a6dcdc6255749d559c24567d4
Matt Boutell <boutell@gmail.com>
2014-06-06 14:24:56 -0400

    Basic main

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+/**
+ * Runs the unit course material generator.
+ *
+ * @author Matt Boutell.
+ *         Created Jun 6, 2014.
+ */
+public class Main {
+
+	/**
+	 * Starts here.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
