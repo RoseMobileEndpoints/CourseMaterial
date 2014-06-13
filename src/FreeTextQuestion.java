1e3974e8479a88829144127c8cf8a9479da0899d
Matt Boutell <boutell@gmail.com>
2014-06-13 18:00:33 -0400

    Stub for freetext questions. Next sprint.

---
+++ src/FreeTextQuestion.java
@@ -0,0 +1,30 @@
+
+/**
+ * A freetext question uses regex.
+ *
+ * @author Matt Boutell.
+ *         Created Jun 13, 2014.
+ */
+public class FreeTextQuestion extends Question {
+
+	@Override
+	public void addQuestionChoice(String line) {
+		// TODO Auto-generated method stub.
+		
+	}
+// Sample:
+/*	
+var activity = [
+
+  { questionType: 'freetext',
+    correctAnswerRegex: /white?/i,
+    correctAnswerOutput: "Correct--Many charts, tables, and graphs have white backgrounds, so filtering for white images helps you find them faster.",
+    incorrectAnswerOutput: "Try again. Consider what color would be dominant in images of charts, tables, and graphs. Look at the results above. Each of those sources is traditionally printed on paper.",
+    showAnswerOutput: "Our search expert says: I would click on white in the color grid, since many charts, tables, and graphs have white backgrounds." 
+  }
+
+];
+*/	
+	
+	
+}
